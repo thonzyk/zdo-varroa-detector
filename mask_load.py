4a81c652b25d681fb5abd07a8e38e20c2cc571ce
sieberm111 <siebermatej@seznam.cz>
2021-03-28 19:57:47 +0200

    edit na pic klestiku na filtraci + tezor masek v nparray

--- mask_load.py
+++ mask_load.py
@@ -6,74 +6,44 @@ import numpy as np
 from matplotlib import pyplot as plt
 matplotlib.use('Qt5Agg')
 
-object_path = Path("C:/Users/poker/Downloads/MASK-sieberm/SegmentationClass/")
-images_path = Path("C:/Users/poker/Downloads/MASK-sieberm/JPEGImages/")
-
 if __name__ == '__main__':
-    #mask = io.imread(object_path / "Original_1305_image.png", as_gray=True)
-    orig = io.imread(images_path / "Original_1305_image.jpg", as_gray=True)
 
     im = cv2.imread('C:/Users/poker/Downloads/MASK-sieberm/SegmentationClass/Original_1305_image.png')
+    orig = cv2.imread('C:/Users/poker/Downloads/MASK-sieberm/JPEGImages/Original_1305_image.jpg')
 
     imgray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
     ret, thresh = cv2.threshold(imgray,20,255,cv2.THRESH_BINARY)
     image, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
     print(len(contours))
 
-
-    # mat_list=[]
-    # for c in contours:
-    #     # compute the center of the contour
-    #     M = cv2.moments(c)
-    #     cX = int(M["m10"] / M["m00"])
-    #     cY = int(M["m01"] / M["m00"])
-    #
-    #     mat=orig[cY-20:cY+20,cX-20:cX+20]
-    #     mat_list.append(mat)
-    #
-    # print(mat_list[0])
-    #
-    # for i in range(0,len(mat_list)):
-    #     name="klest_"+str(i)+".png"
-    #     matplotlib.image.imsave(name, mat_list[i])
-
+# KLESTICI MASKA Z ANOT DAT
     mat_list=[]
     for c in contours:
         # compute the center of the contour
         M = cv2.moments(c)
         cX = int(M["m10"] / M["m00"])
         cY = int(M["m01"] / M["m00"])
-        mat = image[cY - 20:cY + 20, cX - 20:cX + 20].astype("int32")
+        mat = image[cY - 12:cY + 12, cX - 12:cX + 12].astype("int32")
         mat_list.append(mat)
-
-
-    #cv2.imshow('a',mat)
-    #cv2.waitKey(0)
 
     final_mat=sum(mat_list)
 
     template=(final_mat.astype("float32")/19/255)
+    np.save('tmp.npy',template)
 
 
 
-    im_in = cv2.imread('C:/Users/poker/Downloads/MASK-sieberm/JPEGImages/Original_1305_image.jpg')
-    imgray_in =1-cv2.cvtColor(im_in, cv2.COLOR_BGR2GRAY)
+    # template=template/ template.max()
+    # template=template*255
+    # template = template.astype(np.uint8)
+    #
+    #
+    # im_in = cv2.imread('C:/Users/poker/Downloads/MASK-sieberm/JPEGImages/Original_1305_image.jpg')
+    # imgray_in =cv2.cvtColor(im_in, cv2.COLOR_BGR2GRAY)
+    #
+    # res = cv2.matchTemplate(imgray_in,template,cv2.TM_CCORR_NORMED)
+    # #min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+    #
+    # cv2.imshow("a", res)
+    # cv2.waitKey(0)
 
-    #cv2.imshow("a", imgray_in)
-    #cv2.waitKey(0)
-
-    #res = cv2.matchTemplate(imgray_in,template,cv2.TM_CCORR_NORMED)
-    #min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
-
-    #cv2.imshow("a",res)
-    #cv2.waitKey(0)
-
-    #res=res>0.5
-
-    #plt.imshow(res)
-    #plt.show()
-
-    print()
-
-    np.save('tmp',template)
---
+++ predzpracovani/predzpracovani_small.py
@@ -0,0 +1,91 @@
+import os
+import cv2
+import matplotlib
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+def mask_create_add(mask_list):
+    retyped=[]
+    for i in mask_list:
+        retyped.append(i.astype("float32"))
+    template=sum(retyped)/len(mask_list)
+    return template
+
+
+def mask_create_multiply(mask_list):
+    retyped = []
+    for i in mask_list:
+        retyped.append(i.astype("float32"))
+    template=retyped[0]
+    for index in range(1,len(retyped)):
+        template=template*retyped[index]
+    return template
+
+if __name__ == '__main__':
+
+    relevant_path = "C:/Users/poker/Downloads/small_MASK_dataset"
+    extension_jpg = ['jpg']
+    extension_png = ['png']
+    img_names = [fn for fn in os.listdir(relevant_path)
+                  if any(fn.endswith(ext) for ext in extension_jpg)]
+    mask_names = [fn for fn in os.listdir(relevant_path)
+                  if any(fn.endswith(ext) for ext in extension_png)]
+
+    print(img_names)
+    print(mask_names)
+
+    neighborhood=14
+    size_img=2*neighborhood
+
+    mask_list=[]
+    img_list=[]
+
+    for index in range(0,len(mask_names)):
+        im = cv2.imread(relevant_path+'/'+mask_names[index])         #mask
+        imgray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)   #grey mask
+
+        im2 = cv2.imread(relevant_path + '/' + img_names[index])  # img
+
+        ret, thresh = cv2.threshold(imgray,20,255,cv2.THRESH_BINARY)
+        _, countours,_ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        print(len(countours))
+
+        for c in countours:
+            # compute the center of the contour
+            M = cv2.moments(c)
+            cX = int(M["m10"] / M["m00"])
+            cY = int(M["m01"] / M["m00"])
+
+            # vyrez obrazu masky
+            mat = imgray[cY - neighborhood:cY + neighborhood, cX - neighborhood:cX + neighborhood]
+            mask_list.append(mat)
+            mat2=im2[cY - neighborhood:cY + neighborhood, cX - neighborhood:cX + neighborhood]
+            # cv2.imshow('a',mat2)
+            # cv2.waitKey(0)
+            img_list.append(mat2)
+
+np.save('maska_tenzor',mask_list)
+
+full_img_mask=np.zeros(shape=(5*size_img,5*size_img))
+k=0
+for i in range(0,5):
+    for j in range(0, 5):
+        full_img_mask[i*size_img:i*size_img+size_img,j*size_img:j*size_img+size_img]=mask_list[k]
+        k = k + 1
+
+#matplotlib.image.imsave('tenzor_mask.jpg', full_img_mask)
+
+full_img_img=np.zeros(shape=(5*size_img,5*size_img,3))
+k=0
+for i in range(0,5):
+    for j in range(0, 5):
+        full_img_img[i*size_img:i*size_img+size_img,j*size_img:j*size_img+size_img]=img_list[k]
+        k=k+1
+
+#cv2.imwrite('tenzor_klestici.jpg', full_img_img)
+
+tmp1=mask_create_add(mask_list)
+tmp2=mask_create_multiply(mask_list)
+
+print()
