5c521f95cab59d0c5bf2421c4a5b03ca586d222f
sieberm111 <siebermatej@seznam.cz>
2021-04-25 11:03:45 +0200

    tuple hue mask

--- preprocessing/preprocessing_threshold.py
+++ preprocessing/preprocessing_threshold.py
@@ -25,7 +25,7 @@ def template_match_tst(img):
     return out
 
 
-def create_hue_mask(image, lower_color=(0,26,0), upper_color=(255,255,145)):
+def create_hue_mask(image, lower_color=(9,0,110), upper_color=(126,161,255)):
     lower = np.array(lower_color, np.uint8)
     upper = np.array(upper_color, np.uint8)
 
@@ -37,20 +37,20 @@ def create_hue_mask(image, lower_color=(0,26,0), upper_color=(255,255,145)):
     kernel = np.ones((3, 3), np.uint8) #todo definice nova
     mask_invert_e = cv2.erode(mask_invert, kernel, 1)
     mask_invert_d = cv2.dilate(mask_invert_e, kernel, 1)
-    #output_image = cv2.bitwise_and(image, image, mask=mask_invert_d)
-    return mask_invert
+    output_image = cv2.bitwise_and(image, image, mask=mask_invert)
+    return output_image
 
 if __name__== '__main__':
-    #im_klestici=cv2.imread('tenzor_klestici.jpg')
-    im_klestici = cv2.imread('../MASK-sieberm/JPEGImages/Original_1305_image.jpg')
+    im_klestici=cv2.imread('tenzor_klestici.jpg')
+    #im_klestici = cv2.imread('../MASK-sieberm/JPEGImages/Original_1305_image.jpg')
 
     blur_image = cv2.medianBlur(im_klestici, size_of_blur)
     hsv_image = cv2.cvtColor(blur_image, cv2.COLOR_BGR2HSV)
 
     masked_klestici = create_hue_mask(hsv_image)#, setup_invert[0:3], setup_invert[3:6])
-    cv2.imshow('a',masked_klestici)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    # cv2.imshow('a',masked_klestici)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
     masked_image = cv2.cvtColor(masked_klestici, cv2.COLOR_HSV2BGR)
 
     h1, w1 = im_klestici.shape[:2]
